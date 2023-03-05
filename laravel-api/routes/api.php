0f54631569952d7871d10636e4303cf6f317f609
oldman07 <backagain010@gmail.com>
2023-03-05 08:37:13 +0500

    model import

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/home', function () {
+    return 'hi i am from api';
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
